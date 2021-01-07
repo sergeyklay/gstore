08175c08877861e218772fb01cf835feb34348c2
Serghei Iakovlev <egrep@protonmail.ch>
2021-01-08 01:23:03 +0200

    Add test for args.argparse for empty args list

---
+++ tests/test_args.py
@@ -0,0 +1,40 @@
+# Copyright (C) 2020, 2021 Serghei Iakovlev <egrep@protonmail.ch>
+#
+# This file is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+import sys
+from argparse import ArgumentParser
+from unittest import mock
+
+from gstore import args
+
+
+def test_none_args(monkeypatch):
+    """
+    Show help message and return None if gstore was called without any argument
+    and environment variables were not enough to start gstore.
+    """
+    def mock_get_token_from_env():
+        return None
+
+    monkeypatch.setattr('sys.argv', ['gstore'])
+    monkeypatch.setattr(
+        args,
+        'get_token_from_env',
+        mock_get_token_from_env
+    )
+
+    with mock.patch.object(ArgumentParser, 'print_help') as mock_help:
+        assert args.argparse() is None
+        mock_help.assert_called_once_with(sys.stderr)
