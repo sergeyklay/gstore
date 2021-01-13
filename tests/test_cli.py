cf95218edf561249916d8127bd8bcfd09e3e0a9b
Serghei Iakovlev <egrep@protonmail.ch>
2021-01-14 00:01:57 +0200

    Catch BadCredentialsException in Client.get_orgs()

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,12 +13,15 @@
 # You should have received a copy of the GNU General Public License
 # along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
+import logging
 import signal
+from unittest import mock
 
 from gstore import cli, client
 
 
 def test_main_keyboard_interrupt(monkeypatch):
+    """Handle Ctrl-C keyboard event and gracefully terminate work."""
     def mock_init(*args, **kwargs):
         raise KeyboardInterrupt
 
@@ -26,3 +29,29 @@ def test_main_keyboard_interrupt(monkeypatch):
     monkeypatch.setattr(client.Client, '__init__', mock_init)
 
     assert cli.main() == 128 + signal.SIGINT
+
+
+def test_get_orgs_without_token(monkeypatch):
+    """Call with invalid token leads to exit with a status of one."""
+    monkeypatch.setattr('sys.argv', ['gstore', '--token', 'secret'])
+
+    with mock.patch.object(logging.Logger, 'info') as mock_logger:
+        assert cli.main() == 1
+
+        mock_logger.assert_called_once_with(
+            'Getting organizations for user')
+
+
+def test_resolve_orgs_without_token(monkeypatch):
+    """Call with invalid token leads to exit with a status of one."""
+    monkeypatch.setattr('sys.argv', [
+        'gstore',
+        '--token', 'secret',
+        '--org', 'Acme'
+    ])
+
+    with mock.patch.object(logging.Logger, 'info') as mock_logger:
+        assert cli.main() == 1
+
+        mock_logger.assert_called_once_with(
+            'Resolve organizations from provided configuration')
