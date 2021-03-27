2ff0b32a9e0d0b0abfca1d91e5b98cfdd13f1a43
Serghei Iakovlev <egrep@protonmail.ch>
2021-03-27 21:38:59 +0200

    Initial ability to use multiprocessing

--- gstore/repo.py
+++ gstore/repo.py
@@ -16,13 +16,129 @@
 """Repository classes used to wrap git classes for gstore."""
 
 import logging
+import multiprocessing
 import os
 import shutil
+from itertools import zip_longest
 
 import git
 
 from gstore.exceptions import parse_git_errors
 from gstore.models import Organization, Repository
+
+logger = logging.getLogger('gstore.repo_manager')
+
+
+def clone(repo: Repository, target: str):
+    """Clone a repository to the target directory."""
+    logger.info(
+        'Clone repository to %s/%s',
+        repo.org.login,
+        repo.name
+    )
+
+    if os.path.exists(target):
+        os.removedirs(target)
+
+    git_url = 'git@github.com:%s/%s.git' % (repo.org.login, repo.name)
+
+    try:
+        git.Repo.clone_from(
+            git_url,
+            target,
+            progress=RepoProgressPrinter()
+        )
+    except git.GitCommandError as exception:
+        logger.error(
+            'Failed to clone %s/%s',
+            repo.org.login,
+            repo.name
+        )
+        logger.error(exception)
+        for msg in parse_git_errors(exception):
+            logger.error(msg)
+
+
+def fetch(repo: Repository, target: str):
+    """Sync a repository in the target directory."""
+    logger.info(
+        'Update repository in %s/%s',
+        repo.org.login,
+        repo.name
+    )
+
+    logger.debug('Initialize repository instance')
+    local_repo = git.Repo(target)
+
+    if len(local_repo.heads) == 0:
+        logger.info(
+            'There are no remote branches for %s/%s, skip updating',
+            repo.org.login,
+            repo.name
+        )
+        return
+
+    try:
+        logger.debug('Download objects and refs from repository')
+        local_repo.git.fetch(['--prune', '--quiet'])
+
+        logger.debug('Fetch from and integrate with repository')
+        local_repo.git.pull(['--all', '--quiet'])
+    except git.GitCommandError as exception:
+        logger.error(
+            'Failed to update %s/%s',
+            repo.org.login,
+            repo.name
+        )
+        for msg in parse_git_errors(exception):
+            logger.error(msg)
+
+
+def do_sync_fake(params: tuple):
+    repos_list, base_path = params
+
+    logger.error(f'''PID={os.getpid()}''')
+    logger.error(f'''{repos_list}''')
+
+
+def do_sync(params: tuple):
+    repos_list, base_path = params
+
+    for repo in repos_list:
+        org_path = os.path.join(base_path, repo.org.login)
+        repo_path = os.path.join(org_path, repo.name)
+        git_path = os.path.join(repo_path, '.git')
+
+        if os.path.exists(repo_path):
+            if os.path.isfile(repo_path):
+                logger.error(
+                    'Unable to sync %s. The path %s is a regular file',
+                    repo.name,
+                    repo_path,
+                )
+                continue
+
+            if not os.access(repo_path, os.W_OK | os.X_OK):
+                logger.error(
+                    'Unable to sync %s. The path %s is not writeable',
+                    repo.name,
+                    repo_path,
+                )
+                continue
+
+            # We're going to run a Git command, but weren't inside a
+            # local Git repository.
+            if not os.path.exists(git_path):
+                logger.debug(
+                    'Remove wrong formed local repo from %s',
+                    repo_path
+                )
+                shutil.rmtree(repo_path, ignore_errors=True)
+
+        if os.path.exists(git_path):
+            fetch(repo, repo_path)
+        else:
+            clone(repo, repo_path)
 
 
 class RepoProgressPrinter(git.RemoteProgress):
@@ -55,7 +171,7 @@ class RepoProgressPrinter(git.RemoteProgress):
             In case of the 'WRITING' operation, it contains the amount of bytes
             transferred. It may, possibly be used for other purposes as well.
         """
-        logging.getLogger('gstore.repo').debug(
+        logger.debug(
             '%s %s %s %s %s',
             op_code,
             cur_count,
@@ -70,112 +186,30 @@ class RepoManager:
 
     def __init__(self, base_path: str):
         self.base_path = os.path.expanduser(base_path).rstrip('/\\')
-        self.logger = logging.getLogger('gstore.repo_manager')
-
-    def clone(self, repo: Repository, target: str):
-        """Clone a repository to the target directory."""
-        self.logger.info(
-            'Clone repository to %s/%s',
-            repo.org.login,
-            repo.name
-        )
-
-        if os.path.exists(target):
-            os.removedirs(target)
-
-        git_url = 'git@github.com:%s/%s.git' % (repo.org.login, repo.name)
-
-        try:
-            git.Repo.clone_from(
-                git_url,
-                target,
-                progress=RepoProgressPrinter()
-            )
-        except git.GitCommandError as exception:
-            self.logger.error(
-                'Failed to clone %s/%s',
-                repo.org.login,
-                repo.name
-            )
-            for msg in parse_git_errors(exception):
-                self.logger.error(msg)
-
-    def fetch(self, repo: Repository, target: str):
-        """Sync a repository in the target directory."""
-        self.logger.info(
-            'Update repository in %s/%s',
-            repo.org.login,
-            repo.name
-        )
-
-        self.logger.debug('Initialize repository instance')
-        local_repo = git.Repo(target)
-
-        if len(local_repo.heads) == 0:
-            self.logger.info(
-                'There are no remote branches for %s/%s, skip updating',
-                repo.org.login,
-                repo.name
-            )
-            return
-
-        try:
-            self.logger.debug('Download objects and refs from repository')
-            local_repo.git.fetch(['--prune', '--quiet'])
-
-            self.logger.debug('Fetch from and integrate with repository')
-            local_repo.git.pull(['--all', '--quiet'])
-        except git.GitCommandError as exception:
-            self.logger.error(
-                'Failed to update %s/%s',
-                repo.org.login,
-                repo.name
-            )
-            for msg in parse_git_errors(exception):
-                self.logger.error(msg)
 
     def sync(self, org: Organization, repos: list):
         """Sync repositories for an organization."""
-        self.logger.info('Sync repos for %s', org.login)
+        logger.info('Sync repos for %s', org.login)
 
         org_path = os.path.join(self.base_path, org.login)
 
         # Just in case create directories recursively
         if not os.path.exists(org_path):
-            self.logger.debug('Creating directory %s', org_path)
+            logger.debug('Creating directory %s', org_path)
             os.makedirs(org_path)
 
-        for repo in repos:
-            repo_path = os.path.join(org_path, repo.name)
-            git_path = os.path.join(repo_path, '.git')
+        num_proc = multiprocessing.cpu_count()
 
-            if os.path.exists(repo_path):
-                if os.path.isfile(repo_path):
-                    self.logger.error(
-                        'Unable to sync %s. The path %s is a regular file',
-                        repo.name,
-                        repo_path,
-                    )
-                    continue
+        def chunks(lst, n):
+            """Yield successive n-sized chunks from lst."""
+            for i in range(0, len(lst), n):
+                yield lst[i:i + n]
 
-                if not os.access(repo_path, os.W_OK | os.X_OK):
-                    self.logger.error(
-                        'Unable to sync %s. The path %s is not writeable',
-                        repo.name,
-                        repo_path,
-                    )
-                    continue
+        tasks = zip_longest(
+            chunks(repos, num_proc),
+            [],
+            fillvalue=self.base_path,
+        )
 
-                # We're going to run a Git command, but weren't inside a
-                # local Git repository.
-                if not os.path.exists(git_path):
-                    self.logger.debug(
-                        'Remove wrong formed local repo from %s',
-                        repo_path
-                    )
-                    shutil.rmtree(repo_path, ignore_errors=True)
-
-            if os.path.exists(git_path):
-                self.fetch(repo, repo_path)
-            else:
-                self.clone(repo, repo_path)
+        with multiprocessing.Pool(processes=num_proc) as p:
+            p.map(do_sync, tasks)
