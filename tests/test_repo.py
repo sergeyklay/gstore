8b5759593e1004b109b7ade7c6e8750d4dd7b037
Serghei Iakovlev <egrep@protonmail.ch>
2023-04-01 22:32:27 +0200

    Amend repo tests

--- tests/test_repo.py
+++ tests/test_repo.py
@@ -13,11 +13,13 @@
 # You should have received a copy of the GNU General Public License
 # along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
-from unittest.mock import MagicMock
+import os
+from unittest.mock import MagicMock, call
 
 from git import GitCommandError
 
-from gstore.repo import clone, fetch
+from gstore.repo import _do_sync
+from gstore.repo import clone, fetch, sync
 
 
 def test_clone_success(mocker, repository, repo_target, test_context):
@@ -94,3 +96,37 @@ def test_fetch_failure(mocker, repository, repo_target, test_context):
     fetch(repository, repo_target)
 
     test_context.logger.error.assert_called()
+
+
+def test_do_sync(mocker, repository, repo_target, test_context):
+    mocker.patch('gstore.repo._ctx', test_context)
+    mock_clone = mocker.patch('gstore.repo.clone')
+    mock_fetch = mocker.patch('gstore.repo.fetch')
+    repos_list = [repository]
+
+    _do_sync(repos_list)
+
+    org_path = os.path.join(test_context.base_path, repository.org.login)
+    repo_path = os.path.join(org_path, repository.name)
+    mock_clone.assert_called_once_with(repository, repo_path)
+    mock_fetch.assert_not_called()
+
+
+def test_sync(mocker, organization, repository, repo_target, test_context):
+    mocker.patch('gstore.repo._ctx', test_context)
+    mocker.patch('multiprocessing.cpu_count', return_value=4)
+    mock_pool = MagicMock()
+    mock_pool.__enter__.return_value = mock_pool
+    mocker.patch('multiprocessing.Pool', return_value=mock_pool)
+    mocker.patch('os.makedirs')
+
+    repos = [repository]
+    base_path = os.path.dirname(repo_target)
+
+    sync(organization, repos, base_path)
+
+    test_context.logger.info.assert_has_calls(
+        [call('Sync repos for %s', organization.login)])
+    test_context.logger.debug.assert_called_once_with(
+        'Creating directory %s', os.path.join(base_path, organization.login))
+    mock_pool.map.assert_called_once()
